f9a47803f0d99cf33bcfe9f9d86bbfecc1867b91
Owner <joaquinhuarangaorihuela@gmail.com>
2025-11-18 15:00:18 -0500

    update locale to ConfigsInitializer.tsx

--- apps/hosting-admin/src/App.tsx
+++ apps/hosting-admin/src/App.tsx
@@ -6,17 +6,6 @@ import {
 } from "./providers";
 import { Router } from "./router";
 
-import dayjs from "dayjs";
-import weekDay from "dayjs/plugin/weekday";
-import localeData from "dayjs/plugin/localeData";
-import updateLocale from "dayjs/plugin/updateLocale";
-
-dayjs.extend(weekDay);
-dayjs.extend(localeData);
-dayjs.extend(updateLocale);
-
-dayjs.updateLocale("es", { weekStart: 0 });
-
 function App() {
   return (
     <VersionProvider>
