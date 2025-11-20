5461366c93c1524398836d2f70065f11f02603b6
Angel Gala <galafloresangelemilio@gmail.com>
2025-11-20 15:48:27 -0500

    added new component PDFViewer of react pdf render

--- apps/hosting-admin/src/pages/quotations/sheets/index.tsx
+++ apps/hosting-admin/src/pages/quotations/sheets/index.tsx
@@ -1,8 +1,9 @@
-import { PDF, Sheet, Spinner } from "../../../components";
 import { useParams } from "react-router-dom";
-import { QuotationDocumentSheet } from "./QuotationDocumentSheet.tsx";
+import { PDFViewer } from "@react-pdf/renderer";
+import { MyDocument } from "./MyDocument.tsx";
 import { useDocumentData } from "react-firebase-hooks/firestore";
 import { firestore } from "../../../firebase";
+import { Spinner } from "../../../components";
 
 export const QuotationSheets = () => {
   const { quotationId } = useParams();
@@ -15,10 +16,8 @@ export const QuotationSheets = () => {
   if (quotationLoading) return <Spinner height="80vh" />;
 
   return (
-    <PDF>
-      <Sheet>
-        <QuotationDocumentSheet quotation={quotation} />
-      </Sheet>
-    </PDF>
+    <PDFViewer style={{ width: "100vw", height: "100vh", border: "none" }}>
+      <MyDocument quotation={quotation} />
+    </PDFViewer>
   );
 };
