4f3b60925d5529fad50a71577fc6bb39819109de
Angel Gala <85372974+AngelDev2001@users.noreply.github.com>
2025-11-21 13:32:34 -0500

    Refactor services section (#60)
    * updated width of carousel
    
    * refactored services section
    
    * updated width of carousel
    
    * refactored services section
    
    * updated attribute stop-color to stopColor
    
    * title updated to link

--- apps/hosting/components/FooterLayout.tsx
+++ apps/hosting/components/FooterLayout.tsx
@@ -83,8 +83,8 @@ export const FooterLayout = () => {
                       y1="100%"
                       y2="0%"
                     >
-                      <stop offset="0%" stop-color="#1faf38" />
-                      <stop offset="100%" stop-color="#60d669" />
+                      <stop offset="0%" stopColor="#1faf38" />
+                      <stop offset="100%" stopColor="#60d669" />
                     </linearGradient>
                     <linearGradient
                       id="SVGHW6lecxh"
@@ -93,8 +93,8 @@ export const FooterLayout = () => {
                       y1="100%"
                       y2="0%"
                     >
-                      <stop offset="0%" stop-color="#f9f9f9" />
-                      <stop offset="100%" stop-color="#fff" />
+                      <stop offset="0%" stopColor="#f9f9f9" />
+                      <stop offset="100%" stopColor="#fff" />
                     </linearGradient>
                   </defs>
                   <path
