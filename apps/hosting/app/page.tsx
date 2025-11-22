cd58fc30527a655cff2df45c26354e699f0930a9
lourdes <palominolourdes694@gmail.com>
2025-11-21 19:37:49 -0500

    what do we do

--- apps/hosting/app/page.tsx
+++ apps/hosting/app/page.tsx
@@ -3,11 +3,13 @@ import { NewsLetterSection } from "@/sections/NewsLetterSection";
 import { ServicesSection } from "@/sections";
 import { CarouselSection } from "@/sections/CarouselSection";
 import { BrandsSection } from "@/sections/BrandsSection";
+import { WhatDoWeDo } from "@/sections/WhatDoWeDo";
 
 export default function Home() {
   return (
     <main>
       <CarouselSection />
+      <WhatDoWeDo />
       <ServicesSection />
       <BusinessBoostBanner />
       <NewsLetterSection />
