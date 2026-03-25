2296a17c98e6391ce3561e16e73b9a5ca697c251
Angel Gala <galafloresangelemilio@gmail.com>
2026-03-25 12:02:12 -0500

    remove toFixed in subTotal of create quotation page

--- apps/hosting-admin/src/pages/quotations/_quotationId/index.tsx
+++ apps/hosting-admin/src/pages/quotations/_quotationId/index.tsx
@@ -122,7 +122,7 @@ export function QuotationIntegration() {
     solutionAndRecommendations: formData.solutionAndRecommendations,
     quotationDetails: formData.quotationDetails.map((item) => ({
       ...item,
-      subTotal: item.subTotal.toFixed(2),
+      subTotal: item.subTotal,
       description: item.description,
     })),
     contractNumber: dayjs().format("YYYYMMDDHHmmss"),
