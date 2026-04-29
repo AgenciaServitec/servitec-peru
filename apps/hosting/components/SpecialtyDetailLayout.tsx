d8c1ad8e905fd8c904a7a4e6087f80777612d75e
Owner <joaquinhuarangaorihuela@gmail.com>
2026-04-29 16:38:53 -0500

    change hablemos to llamanos

--- apps/hosting/components/SpecialtyDetailLayout.tsx
+++ apps/hosting/components/SpecialtyDetailLayout.tsx
@@ -63,7 +63,7 @@ export default function SpecialtyDetailLayout({
               icon={Phone}
               variant="secondary"
             >
-              Hablemos
+              Llamanos
             </ButtonLink>
           </div>
         </ContentWidth>
